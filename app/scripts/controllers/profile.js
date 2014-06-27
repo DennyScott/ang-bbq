d0e82ead7f30c687cba43cb597acfb89ea71fe90
Denny Scott <denny@boldapps.net>
2014-06-27 09:32:37 -0500

    Added Connection

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -1,20 +1,32 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name bbiqApp.controller:ProfileCtrl
- * @description
- * # ProfileCtrl
- * Controller of the bbiqApp
- */
+* @ngdoc function
+* @name bbiqApp.controller:ProfileCtrl
+* @description
+* # ProfileCtrl
+* Controller of the bbiqApp
+*/
 angular.module('bbiqApp')
-  .controller('ProfileCtrl', function ($scope, $location, userFactory) {
-		$('#content-header').html('Profile');  
-		$scope.users = userFactory.query();
-		alert($scope.users);
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+ .controller('ProfileCtrl', ['$scope','$http','$rootScope','dataFactory', function ($scope, $http, $rootScope, dataFactory) {
+               $('#content-header').html('Profile');  
+
+   getUser(1);
+
+   function getUser(id) {
+       dataFactory.getUser(id)
+           .success(function (custs) {
+               $scope.user = custs;
+           })
+           .error(function (error) {
+               $scope.status = 'Unable to load user data: ' + error.message;
+           });
+   }
+
+
+   $scope.awesomeThings = [
+     'HTML5 Boilerplate',
+     'AngularJS',
+     'Karma'
+   ];
+ }]);
--- app/scripts/resources/users.js
+++ app/scripts/resources/users.js
@@ -1,14 +1,38 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name bbiqApp.controller:ProfileCtrl
- * @description
- * # ProfileCtrl
- * Controller of the bbiqApp
- */
+* @ngdoc function
+* @name bbiqApp.controller:ProfileCtrl
+* @description
+* # ProfileCtrl
+* Controller of the bbiqApp
+*/
 angular.module('bbiqApp')
-  .factory('userFactory', function($http, $resource){
-		$http.defaults.useXDomain = true;
-		return $resource('http://104.131.250.11/user/:id', { id:'@_id' });
-	});
+   .factory('dataFactory', ['$http', function($http) {
+
+   var urlBase = 'http://104.131.250.11/user';
+   var dataFactory = {};
+
+   dataFactory.getUsers = function () {
+       return $http.get(urlBase);
+   };
+
+   dataFactory.getUser = function (id) {
+       return $http.get(urlBase + '/' + id);
+   };
+
+   dataFactory.insertUser = function (cust) {
+       return $http.post(urlBase, cust);
+   };
+
+   dataFactory.updateUSer = function (cust) {
+       return $http.put(urlBase + '/' + cust.ID, cust)
+   };
+
+   dataFactory.deleteUser = function (id) {
+       return $http.delete(urlBase + '/' + id);
+   };
+
+   console.log(dataFactory);
+   return dataFactory;
+}]);
