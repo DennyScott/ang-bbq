2938a5e269c3634b466695634839f4b654af9032
Denny Scott <denny@boldapps.net>
2014-06-27 09:14:43 -0500

    temp fix

--- app/scripts/resources/users.js
+++ app/scripts/resources/users.js
@@ -7,7 +7,4 @@
  * # ProfileCtrl
  * Controller of the bbiqApp
  */
-angular.module('bbiqApp')
-  .factory('userFactory', function($resource){
-		return $resource('http://104.131.250.11/user/:id', { id:'@_id' });
-	});
+
